9bd3c40bdae3c82131d26fefe79b16e3bb1f302a
Ankitkr007 <105605443+Ankitkr007@users.noreply.github.com>
2023-01-20 13:18:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import requests
 import streamlit as st
 import pickle
-import numpy as np
-import pandas as pd
 
 
 
 
 
 
-pickle_in=open('model5.pkl','rb')
+pickle_in=open("C:/Users/hp/Downloads/model5.pkl","rb")
 model5=pickle.load(pickle_in)
 
 
-def predict_fraud_payment_detection(step,type,amount,oldbalanceOrig,newbalanceOrig,oldbalanceDest,newbalanceDest):
-    prediction=model5.predict([[step,type,amount,oldbalanceOrig,newbalanceOrig,oldbalanceDest,newbalanceDest]])
+
+
+
+
+
+def predict_fraud_payment_detection(step,types,amount,oldbalanceOrig,newbalanceOrig,oldbalanceDest,newbalanceDest):
+    prediction=model5.predict([[step,types,amount,oldbalanceOrig,newbalanceOrig,oldbalanceDest,newbalanceDest]])
     print(prediction)
     return prediction
 
@@ -22,12 +25,14 @@ def predict_fraud_payment_detection(step,type,amount,oldbalanceOrig,newbalanceOr
 def main():
     st.title("Credit Card Payment Fraud Detector")
     html_temp="""
-    <div style="background-color:tomato;padding:10px>
-    <h2 style='color:white;text-align:center;">Streamlit Credit Card Fraud Detector ML App </h2>
+    <div style="background-color:cyan;padding:10px>
+    <h2 style='color:white;text-align:center;">Credit Card Fraud Detector App </h2>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <i class="fas fa-credit-card"></i>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
     step = st.text_input("step", "Type Here")
-    type = st.text_input("type", "Type Here")
+    types = st.text_input("types", "Type Here")
     amount = st.text_input("amount", "Type Here")
     oldbalanceOrig = st.text_input("oldbalanceOrig", "Type Here")
     newbalanceOrig = st.text_input("newbalanceOrig", "Type Here")
@@ -36,8 +41,8 @@ def main():
 
     result = ""
     if st.button("Predict"):
-        result =predict_fraud_payment_detection(step,type,amount,oldbalanceOrig,newbalanceOrig,oldbalanceDest,newbalanceDest)
-    st.success('The output is {}'.format(result))
+        result =predict_fraud_payment_detection(step,types,amount,oldbalanceOrig,newbalanceOrig,oldbalanceDest,newbalanceDest)
+    st.success('Here is the output {}'.format(result))
     if st.button("About"):
         st.text("Lets LEarn")
         st.text("Built with Streamlit")
